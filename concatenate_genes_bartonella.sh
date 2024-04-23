c7bb72a843a9201522e815e87c3cf58695d6087f
reymonera <castillo.vilcahuaman@gmail.com>
2024-04-23 02:50:22 -0500

    functional concatenation as needed

--- concatenate_genes_bartonella.sh
+++ concatenate_genes_bartonella.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Parses arguments from the command line, which are four
-# Usage: bash script.sh -list list.txt -db db.fasta -output output.fasta -tree
+# Usage: bash concatenate_genes_bartonella.sh -list list.txt -db db.fasta -output output.fasta -tree
 # This assumes that there is a list.txt file, in which all ids are listed.
-while getopts ":list:db:output:tree" opt; do
+while getopts ":l:d:o:t" opt; do
   case ${opt} in
-    list ) # process list file
+    l ) # process list file
       listfile=$OPTARG
       ;;
-    db ) # process database file
+    d ) # process database file
       dbfile=$OPTARG
       ;;
-    output ) # process output file
+    o ) # process output file
       outputfile=$OPTARG
       ;;
-    tree ) # process tree flag
+    t ) # process tree flag
       treeflag=1
       ;;
-    \? ) echo "Usage: cmd [-list listfile] [-db dbfile] [-output outputfile] [-tree]"
+    \? ) echo "Usage: cmd [-l listfile] [-d dbfile] [-o outputfile] [-t]"
       ;;
   esac
 done
@@ -33,9 +33,7 @@ if [[ ! -e $dbfile ]]; then
     exit 1
 fi
 
-# Initialize variables
-declare -a ids=()
-outfile=""
+echo "Initializing extracting proccess. This will create files for each list"
 
 # Read list file line by line
 while IFS= read -r line
@@ -44,7 +42,8 @@ do
   if [[ $line == "---" ]]; then
     # If ids array is not empty, extract sequences and write to file
     if [[ ${#ids[@]} -ne 0 ]]; then
-      seqkit grep -f <(printf '%s\n' "${ids[@]}") $dbfile > $outfile
+      outfile="${ids[0]}.fasta"
+      seqkit grep -r -f <(printf '%s\n' "${ids[@]}") "$dbfile" -o "$outfile"
     fi
     # Clear ids array
     ids=()
@@ -52,15 +51,49 @@ do
     # Add id to ids array
     ids+=("$line")
     # If outfile is empty, set it to the first id
-    if [[ -z $outfile ]]; then
-      outfile="${line#>}.fasta"
-    fi
+    #if [[ -z $outfile ]]; then
+    #  outfile="${line#>}.fasta"
+    #fi
   fi
 done < "$listfile"
 
 # Handle last list
 if [[ ${#ids[@]} -ne 0 ]]; then
+  outfile="${ids[0]}.fasta"
   seqkit grep -f <(printf '%s\n' "${ids[@]}") $dbfile > $outfile
+fi
+
+echo "Initializing concatenation proccess. This will create temporal files for each sequence"
+
+# Initialize variables
+declare -a ids=()
+outfile=""
+
+while IFS= read -r line
+do
+  # Check if line is a new list indicator
+  if [[ $line == "---" ]]; then
+    # If ids array is not empty, extract sequences and write to file
+    if [[ ${#ids[@]} -ne 0 ]]; then
+      for id in "${ids[@]}"; do
+        outfile="${id}.fa1"
+        echo "$id" | seqkit grep -r -f /dev/stdin "$dbfile" -o "$outfile"
+      done
+    fi
+    # Clear ids array
+    ids=()
+  else
+    # Add id to ids array
+    ids+=("$line")
+  fi
+done < "$listfile"
+
+# Handle last list
+if [[ ${#ids[@]} -ne 0 ]]; then
+  for id in "${ids[@]}"; do
+    outfile="${id}.fa1"
+    echo "$id" | seqkit grep -r -f /dev/stdin "$dbfile" -o "$outfile"
+  done
 fi
 
 for file in *.fasta
@@ -69,15 +102,29 @@ do
     echo "$file: $(grep -c "^>" "$file") sequences"
 done
 
+# Since all FASTA sequences have an "_" and it is necessary to delete it
+# for the concatenation to work, this section will refactor all sequences
+# on the fasta files
+for fasta_file in *.fa1
+do
+    sed -i 's/_/ /g' "$fasta_file"
+done
+
 # Concatenate all .fasta files
 # This uses seqkit
-seqkit concat *.fasta > $outputfile
+seqkit concat --full *.fa1 > $outputfile
+#seqkit concat *.fasta > $outputfile
 
 # Print number of sequences in output file
 echo "Number of sequences in output file: $(grep -c "^>" $outputfile)"
+# Then it just erases every created .fa file to do it tidily
+rm *.fa1
 
 # If tree flag is set, align sequences and generate tree
 if [[ $treeflag -eq 1 ]]; then
+  source /home/marlen/anaconda3/etc/profile.d/conda.sh
+  conda activate mafft
+  conda activate iqtree
   mafft --auto $outputfile > aligned.fasta
   iqtree -s aligned.fasta
 fi
